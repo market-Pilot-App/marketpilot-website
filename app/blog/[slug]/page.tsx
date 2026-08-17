12d5b8b9cb91ad7adf623d1ac34641fd15205267
AltarCloud <altercloud007@gmail.com>
2026-08-17 12:06:14 +0100

    fix: move posts data to shared file, fix blog page exports

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import { posts } from "../page";
+import { posts } from "../posts-data";
 
 const postContent: Record<string, { intro: string; sections: { heading: string; body: string }[] }> = {
   "why-african-businesses-need-social-media-automation": {
